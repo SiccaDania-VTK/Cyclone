bb4dbb76c4cfd2786f126b448b0e2c0643934568
GerritPathuis <GerritPathuis@users.noreply.github.com>
2021-08-31 09:47:34 +0200

    Disk FKo added

--- VTK-cyclone/Form1.vb
+++ VTK-cyclone/Form1.vb
@@ -461,7 +461,8 @@ Public Class Form1
 
         user_list.Add("FredKo")
         'hard_disk_list.Add("JR10006P02Y6EE")       'VTK laptop, FKo
-        hard_disk_list.Add("0025_3851_91B1_072E.")  'VTK new laptop, FKo
+        'hard_disk_list.Add("0025_3851_91B1_072E.")  'VTK new laptop, FKo
+        hard_disk_list.Add("UGXVK01J1BBS7O")  'VTK new laptop, FKo
 
         user_list.Add("JanK")
         hard_disk_list.Add("0025_38B4_71B4_88FC.")  'VTK laptop, Jank
